6cc8b8324e4cb2a4af625cb932b665a5ee452162
Rafael Alba <rafalba.o5@gmail.com>
2022-06-17 15:39:45 -0500

    Se utiliza innerText para cambio de h2

--- html/saludo/script.js
+++ html/saludo/script.js
@@ -3,17 +3,13 @@ console.log("Me parece una falta de respeto");
 
 setInterval(cambiarSaludo, 2000);
 
+// Cambia la frase en el titulo h2
 function cambiarSaludo() {
-    //console.log("Me parece una falta de respecto");
-    // Estoy asignando a una variable de h2
-    let frase = document.getElementsByTagName("h2")[1].innerHTML;
-
-    if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
-        //frase = "Impongo moda para las niñas!";
-        document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+    let frase = document.getElementsByTagName("h2")[1];
+    if (frase.innerText == "¡Ganando como siempre!") {
+        frase.innerText = "¡Impongo moda para las niñas!";
     } else {
-        //frase = "Ganando como siempre";
-        document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+        .innerText = "¡Ganando como siempre!";
     }
-    console.log("frase :" + frase);
+    //console.log("frase :" + frase);
 }
