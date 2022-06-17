fc1d6c567fa27d4a865307863a855b6e7ff41a47
Rafael Alba <rafalba.o5@gmail.com>
2022-06-17 15:10:33 -0500

    se realiza el cambio dinámico de h2

--- html/saludo/script.js
+++ html/saludo/script.js
@@ -1,1 +1,19 @@
 console.log("Me parece una falta de respeto");
+
+
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo() {
+    //console.log("Me parece una falta de respecto");
+    // Estoy asignando a una variable de h2
+    let frase = document.getElementsByTagName("h2")[1].innerHTML;
+
+    if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+        //frase = "Impongo moda para las niñas!";
+        document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+    } else {
+        //frase = "Ganando como siempre";
+        document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+    }
+    console.log("frase :" + frase);
+}
